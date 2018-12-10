0c0e9f2dcf2bb731598373f3f5efaadcba77dc55
Roberto Amaya <betoamaya@msn.com>
2018-12-10 07:43:21 -0600

    Agregando UUID a SP

--- SQLCXC/SQLCXC/Consultar directamente la tabla.sql
+++ SQLCXC/SQLCXC/Consultar directamente la tabla.sql
@@ -1,6 +1,15 @@
-SELECT *
+SELECT c.Ejercicio,
+       c.Periodo,
+       *
 FROM dbo.CFD AS c
-WHERE c.MovID = 'TPAS665363';
+WHERE c.MovID = '154';
+
+SELECT c.Ejercicio,
+       c.Periodo,
+       c.Saldo,
+       *
+FROM dbo.Cxc AS c
+WHERE c.MovID = '154';
 
 SELECT VerAuxCorte.Moneda,
        VerAuxCorte.Cuenta,
@@ -39,4 +48,57 @@ WHERE VerAuxCorte.Estacion = 10000
                                )
 ORDER BY Cte.Cliente,
          VerAuxCorte.Mov,
+         Cxc.FechaEmision DESC;
+
+
+/*******************************************************************************************************************************/
+GO
+SELECT *
+FROM VerAuxCorte
+WHERE Estacion = 10000
+      AND Empresa = 'tun'
+      AND Mov NOT IN ( 'Solicitud Deposito', 'Redondeo', 'CFD Anticipo', 'Ing de Empleado Cred',
+                       'CFD Anticipo ServCom', 'Factura Ant AF SI', 'Factura Anticipos VE', 'Factura Anticipo AF' /*RAAM-15/11/2018 - Se Agrego mas movimientos de la C.P. Zoraida*/
+                     )
+      AND Saldo > 0.9999;
+
+SELECT vac.Moneda,
+       vac.Cuenta,
+       vac.Mov,
+       vac.MovID,
+       vac.Saldo,
+       Cxc.FechaEmision,
+       Cxc.Referencia,
+       Cxc.Vencimiento,
+       Cte.Cliente,
+       Cte.Nombre,
+       Cxc.ClienteEnviarA AS Sucursal, /*2018-10-31 -> Se Agrego Sucursal por Solicitud de la C.P. Zoraida*/
+       Cfd.FechaTimbrado,              /*2018-10-30 -> Se Agrego Fecha XML por Solicitud de la C.P. Zoraida*/
+       Cfd.UUID                        /*2018-12-07 -> Se Agrego Fecha XML por Solicitud de la C.P. Zoraida*/
+FROM dbo.VerAuxCorte AS vac
+    LEFT OUTER JOIN Cxc
+        ON vac.ModuloID = Cxc.ID
+    LEFT JOIN dbo.Venta AS vtas
+        ON vtas.MovID = vac.MovID
+           AND vtas.Mov = vac.Mov
+    LEFT JOIN dbo.CFD AS Cfd
+        ON Cxc.MovID = Cfd.MovID
+           AND (CASE
+                    WHEN ISNULL(Cxc.OrigenTipo, 'CXC') = 'CXC' THEN
+                        Cxc.ID
+                    WHEN Cxc.OrigenTipo = 'VTAS' THEN
+                        vtas.ID
+                END
+               ) = Cfd.ModuloID
+    JOIN Cte
+        ON vac.Cuenta = Cte.Cliente
+WHERE vac.Estacion = 10000
+      AND vac.Empresa = 'tun'
+      AND vac.Mov NOT IN ( 'Solicitud Deposito', 'Redondeo', 'CFD Anticipo', 'Ing de Empleado Cred',
+                           'CFD Anticipo ServCom', 'Factura Ant AF SI', 'Factura Anticipos VE', 'Factura Anticipo AF' /*RAAM-15/11/2018 - Se Agrego mas movimientos de la C.P. Zoraida*/
+                         )
+      AND vac.Saldo > 0.9999 /*RAAM-15/11/2018 - Se Agrego filtro para descartar saldo menor a un peso de la C.P. Zoraida*/
+--AND dbo.VerAuxCorte.MovID = 'TPAS637974'
+ORDER BY Cte.Cliente,
+         vac.Mov,
          Cxc.FechaEmision DESC;
--- SQLCXC/SQLCXC/Ejecutar repSaldoClientes.sql
+++ SQLCXC/SQLCXC/Ejecutar repSaldoClientes.sql
@@ -1,4 +1,4 @@
 EXEC dbo.repSaldoClientes @sEmpresa = 'TUN',          -- char(5)
-                          @dInicio = '2018-11-01', -- date
+                          @dInicio = '2010-01-01', -- date
                           @dFin = '2018-11-30',    -- date
                           @sCliente = NULL           -- varchar(10)
