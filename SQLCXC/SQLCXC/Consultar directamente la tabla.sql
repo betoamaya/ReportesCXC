8474ae4e85a6caf85e10830b7930dc4732d5b562
Roberto Amaya <betoamaya@msn.com>
2018-12-12 10:29:53 -0600

    Saldos por Días

--- SQLCXC/SQLCXC/Consultar directamente la tabla.sql
+++ SQLCXC/SQLCXC/Consultar directamente la tabla.sql
@@ -120,10 +120,10 @@ SELECT vac.Moneda,
        Cte.Nombre,
        Cxc.ClienteEnviarA AS Sucursal, /*2018-10-31 -> Se Agrego Sucursal por Solicitud de la C.P. Zoraida*/
        Cfd.FechaTimbrado,              /*2018-10-30 -> Se Agrego Fecha XML por Solicitud de la C.P. Zoraida*/
-       Cfd.UUID,                        /*2018-12-07 -> Se Agrego Fecha XML por Solicitud de la C.P. Zoraida*/
-	   c.Cuenta,
-	   c2.Descripcion,
-	   datediff(day, Cxc.FechaEmision, '2018-11-30') AS dias
+       Cfd.UUID,                       /*2018-12-07 -> Se Agrego Fecha XML por Solicitud de la C.P. Zoraida*/
+       c.Cuenta,
+       c2.Descripcion,
+       DATEDIFF(DAY, Cxc.FechaEmision, '2018-11-30') AS dias
 FROM dbo.VerAuxCorte AS vac
     LEFT OUTER JOIN Cxc
         ON vac.ModuloID = Cxc.ID
@@ -141,15 +141,81 @@ FROM dbo.VerAuxCorte AS vac
                ) = Cfd.ModuloID
     JOIN Cte
         ON vac.Cuenta = Cte.Cliente
-	left JOIN dbo.Concepto AS c ON c.Concepto = Cxc.Concepto AND c.Modulo = 'VTAS'
-	LEFT JOIN dbo.Cta AS c2 ON c2.Cuenta = c.Cuenta
+    LEFT JOIN dbo.Concepto AS c
+        ON c.Concepto = Cxc.Concepto
+           AND (CASE
+                    WHEN ISNULL(Cxc.OrigenTipo, '') = '' THEN
+                        'CXC'
+                    WHEN Cxc.Mov = 'Int Vta AF' THEN
+                        'CXC'
+                    ELSE
+                        Cxc.OrigenTipo
+                END
+               ) = c.Modulo
+    LEFT JOIN dbo.Cta AS c2
+        ON c2.Cuenta = c.Cuenta
 WHERE vac.Estacion = 10000
       AND vac.Empresa = 'tun'
-      AND vac.Mov NOT IN ( 'Solicitud Deposito', 'Redondeo', 'CFD Anticipo', 'Ing de Empleado Cred',
-                           'CFD Anticipo ServCom', 'Factura Ant AF SI', 'Factura Anticipos VE', 'Factura Anticipo AF' /*RAAM-15/11/2018 - Se Agrego mas movimientos de la C.P. Zoraida*/
+      AND vac.Mov NOT IN (   'Solicitud Deposito', 'Redondeo', 'CFD Anticipo', 'Ing de Empleado Cred',
+                             'CFD Anticipo ServCom', 'Factura Ant AF SI', 'Factura Anticipos VE',
+                             'Factura Anticipo AF', /*RAAM-15/11/2018 - Se Agrego mas movimientos de la C.P. Zoraida*/
+                             'Int Dev x Cob Banco', 'Saldo a Favor' /*RAAM-12/12/2018 - C.P. Solicito quitar estos movimientos*/
                          )
       AND vac.Saldo > 0.9999 /*RAAM-15/11/2018 - Se Agrego filtro para descartar saldo menor a un peso de la C.P. Zoraida*/
---AND dbo.VerAuxCorte.MovID = 'TPAS637974'
+      --AND dbo.VerAuxCorte.MovID = 'TPAS637974'
+      AND (
+              Cxc.MovID IN ( '153', '154', '42', '2016-667', '155', '148' )
+              AND Cxc.Mov = 'Prestamo' AND vac.Estacion = 10000
+          )
+      OR (
+             Cxc.MovID IN ( 'TOT226937' )
+             AND Cxc.Mov = 'Int Vta AF' AND vac.Estacion = 10000
+         )
+      OR (
+             Cxc.MovID IN ( 'LVE5210' )
+             AND Cxc.Mov = 'FACT.VE.GRAVADO' AND vac.Estacion = 10000
+         )
+      OR (
+             Cxc.MovID IN ( 'UPAS87181', 'UPAS87175' )
+             AND Cxc.Mov = 'Bol Pas Credito' AND vac.Estacion = 10000
+         )
+      OR (
+             Cxc.MovID IN ( 'TOT96621', 'TOT96663', 'TOT96665', 'TOT96669' )
+             AND Cxc.Mov = 'NOTA CARGO' AND vac.Estacion = 10000
+         )
+	OR (
+             Cxc.MovID IN ( 'TPAS655028' )
+             AND Cxc.Mov = 'FACT PAS CREDITO' AND vac.Estacion = 10000
+         )
+
+
+---AND Cte.Cliente= '23629'
 ORDER BY Cte.Cliente,
          vac.Mov,
          Cxc.FechaEmision DESC;
+
+/***************************************************/
+SELECT c.OrigenTipo, * FROM dbo.Cxc AS c WHERE c.MovID = 'TPAS655028'
+SELECT * FROM dbo.Concepto AS c WHERE c.Concepto = 'DEUDORES AJENOS'
+
+/******************************/
+SELECT * FROM cxc WHERE cxc.MovID in ('TOT226937') AND cxc.Mov = 'Int Vta AF'
+SELECT * FROM dbo.Concepto AS c WHERE c.Concepto = 'INTERESES LP GRAV 15%'
+
+/******************************/
+SELECT * FROM dbo.Cxc WHERE cxc.MovID = 'LVE5210' AND Mov = 'FACT.VE.GRAVADO'
+SELECT * FROM dbo.Concepto AS c WHERE c.Concepto like 'VIAJE ESPECIAL GRAVA%'
+
+
+/****************************/
+
+SELECT *  FROM cxc WHERE Cxc.MovID IN ( 'UPAS87181', 'UPAS87175' )
+             AND Cxc.Mov = 'Bol Pas Credito'
+SELECT * FROM dbo.Concepto AS c WHERE c.Concepto = 'PASAJES CONTADO BOLETERA'
+SELECT * FROM dbo.Concepto AS c WHERE c.Cuenta = '101-010-102'
+
+/*********************/
+SELECT *  FROM cxc WHERE Cxc.MovID IN ( 'TPAS655028' )
+             AND Cxc.Mov = 'Fact Pas Credito'
+SELECT * FROM dbo.Concepto AS c WHERE c.Cuenta = '101-015-311'
+SELECT * FROM dbo.Cta AS c WHERE c.Cuenta = '101-015-311'
---
+++ SQLCXC/SQLCXC/Saldos por Dias.sql
@@ -0,0 +1,144 @@
+
+--SELECT vac.Moneda,
+--       vac.Cuenta,
+--       vac.Mov,
+--       vac.MovID,
+--       vac.Saldo,
+--       Cxc.FechaEmision,
+--       Cxc.Referencia,
+--       Cxc.Vencimiento,
+--       Cte.Cliente,
+--       Cte.Nombre,
+--       Cxc.ClienteEnviarA AS Sucursal, /*2018-10-31 -> Se Agrego Sucursal por Solicitud de la C.P. Zoraida*/
+--       Cfd.FechaTimbrado,              /*2018-10-30 -> Se Agrego Fecha XML por Solicitud de la C.P. Zoraida*/
+--       Cfd.UUID,                       /*2018-12-07 -> Se Agrego Fecha XML por Solicitud de la C.P. Zoraida*/
+--       c.Cuenta,
+--       c2.Descripcion,
+--       DATEDIFF(DAY, Cxc.FechaEmision, '2018-11-30') AS dias
+
+SELECT tmp.Cuenta,
+       c3.Descripcion,
+       tmp.Cliente,
+       Cte.Nombre,
+       SUM(tmp.Saldo) AS 'Saldo',
+       SUM(tmp.[Saldo al Corriente]) AS 'Saldo al Corriente',
+       SUM(tmp.[Saldo + 30]) AS 'Saldo + 30',
+       SUM(tmp.[Saldo + 60]) AS 'Saldo + 60',
+       SUM(tmp.[Saldo + 90]) AS 'Saldo + 90'
+FROM
+(
+    SELECT Cte.Cliente,
+           c.Cuenta,
+           vac.MovID,
+           SUM(vac.Saldo) AS Saldo,
+           SUM(   CASE
+                      WHEN DATEDIFF(DAY, Cxc.FechaEmision, '2018-11-30')
+                           BETWEEN 0 AND 29 THEN
+                          vac.Saldo
+                      ELSE
+                          0
+                  END
+              ) AS 'Saldo al Corriente',
+           SUM(   CASE
+                      WHEN DATEDIFF(DAY, Cxc.FechaEmision, '2018-11-30')
+                           BETWEEN 30 AND 59 THEN
+                          vac.Saldo
+                      ELSE
+                          0
+                  END
+              ) AS 'Saldo + 30',
+           SUM(   CASE
+                      WHEN DATEDIFF(DAY, Cxc.FechaEmision, '2018-11-30')
+                           BETWEEN 60 AND 89 THEN
+                          vac.Saldo
+                      ELSE
+                          0
+                  END
+              ) AS 'Saldo + 60',
+           SUM(   CASE
+                      WHEN DATEDIFF(DAY, Cxc.FechaEmision, '2018-11-30') >= 90 THEN
+                          vac.Saldo
+                      ELSE
+                          0
+                  END
+              ) AS 'Saldo + 90'
+    FROM dbo.VerAuxCorte AS vac
+        LEFT OUTER JOIN Cxc
+            ON vac.ModuloID = Cxc.ID
+        LEFT JOIN dbo.Venta AS vtas
+            ON vtas.MovID = vac.MovID
+               AND vtas.Mov = vac.Mov
+        LEFT JOIN dbo.CFD AS Cfd
+            ON Cxc.MovID = Cfd.MovID
+               AND (CASE
+                        WHEN ISNULL(Cxc.OrigenTipo, 'CXC') = 'CXC' THEN
+                            Cxc.ID
+                        WHEN Cxc.OrigenTipo = 'VTAS' THEN
+                            vtas.ID
+                    END
+                   ) = Cfd.ModuloID
+        JOIN Cte
+            ON vac.Cuenta = Cte.Cliente
+        LEFT JOIN dbo.Concepto AS c
+            ON c.Concepto = Cxc.Concepto
+               AND (CASE
+                        WHEN ISNULL(Cxc.OrigenTipo, '') = '' THEN
+                            'CXC'
+                        WHEN Cxc.Mov = 'Int Vta AF' THEN
+                            'CXC'
+                        ELSE
+                            Cxc.OrigenTipo
+                    END
+                   ) = c.Modulo
+        LEFT JOIN dbo.Cta AS c2
+            ON c2.Cuenta = c.Cuenta
+    WHERE vac.Estacion = 10000
+          AND vac.Empresa = 'tun'
+          AND vac.Mov NOT IN (   'Solicitud Deposito', 'Redondeo', 'CFD Anticipo', 'Ing de Empleado Cred',
+                                 'CFD Anticipo ServCom', 'Factura Ant AF SI', 'Factura Anticipos VE',
+                                 'Factura Anticipo AF', /*RAAM-15/11/2018 - Se Agrego mas movimientos de la C.P. Zoraida*/
+                                 'Int Dev x Cob Banco', 'Saldo a Favor'
+                             )
+          AND vac.Saldo > 0.9999 /*RAAM-15/11/2018 - Se Agrego filtro para descartar saldo menor a un peso de la C.P. Zoraida*/
+    --AND dbo.VerAuxCorte.MovID = 'TPAS637974'
+    --AND (
+    --        Cxc.MovID IN ( '153', '154', '42', '2016-667', '155', '148' )
+    --        AND Cxc.Mov = 'Prestamo'
+    --    )
+    --OR (
+    --       Cxc.MovID IN ( 'TOT226937' )
+    --       AND Cxc.Mov = 'Int Vta AF'
+    --   )
+    --OR (
+    --       Cxc.MovID IN ( 'LVE5210' )
+    --       AND Cxc.Mov = 'FACT.VE.GRAVADO'
+    --   )
+    --OR (
+    --       Cxc.MovID IN ( 'UPAS87181', 'UPAS87175' )
+    --       AND Cxc.Mov = 'Bol Pas Credito'
+    --   )
+    --OR (
+    --       Cxc.MovID IN ( 'TOT96621', 'TOT96663', 'TOT96665', 'TOT96669' )
+    --       AND Cxc.Mov = 'NOTA CARGO'
+    --       AND vac.Estacion = 10000
+    --   )
+    -- AND Cte.Cliente= '23629'     
+
+    GROUP BY Cte.Cliente,
+             c.Cuenta,
+             vac.MovID
+) AS tmp
+    LEFT JOIN Cte
+        ON tmp.Cliente = Cte.Cliente
+    LEFT JOIN dbo.Cta AS c3
+        ON tmp.Cuenta = c3.Cuenta
+GROUP BY tmp.Cuenta,
+         c3.Descripcion,
+         tmp.Cliente,
+         Cte.Nombre
+ORDER BY tmp.Cuenta,
+         tmp.Cliente ASC;
+
+--ORDER BY Cte.Cliente,
+--         vac.Mov,
+--         Cxc.FechaEmision DESC;
